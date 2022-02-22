b78c928cdbc2c7cd78b19b176194637756b85288
Nikolui333 <semenovnikolu@yandex.ru>
2022-02-22 17:55:24 +0300

    main

--- app/src/main/java/com/example/roommovie/MainActivity.kt
+++ app/src/main/java/com/example/roommovie/MainActivity.kt
@@ -2,18 +2,41 @@ package com.example.roommovie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.KeyEvent
-import android.view.View
-import android.widget.ArrayAdapter
+import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
 import com.example.roommovie.databinding.ActivityMainBinding
+import com.example.roommovie.tabs.TabCategories
+import com.example.roommovie.tabs.TabFilms
+import com.example.roommovie.tabs.TabFilters
+import com.example.roommovie.tabs.TabPanel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     private var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        supportFragmentManager.beginTransaction().replace(R.id.content, TabPanel()).commit()
+
+        binding?.bottomNav?.setOnNavigationItemSelectedListener(this)
+        binding?.bottomNav?.selectedItemId = R.id.panelItemBottomNav
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.panelItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, TabPanel()).commit()
+            R.id.catalogProductsItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, TabFilms()).commit()
+            R.id.catalogClothesItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, TabFilters()).commit()
+            R.id.catalogCategoriesItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, TabCategories()).commit()
+        }
+
+        return true
     }
 }
